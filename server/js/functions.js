8d7c1ed6785b59de8030e0d84d384fb3a70c9227
Leonardo Porto Penna <41974257+AnarcoOldSkull@users.noreply.github.com>
2023-10-18 16:42:15 -0300

    Going to drect to save files but not yet!!

--- server/js/functions.js
+++ server/js/functions.js
@@ -1,3 +1,25 @@
+//const information = fs.readFileSync("log/gamelist.json");
+//console.log(information);
+function criarProjeto(){
+    document.getElementById("executandoProjeto").innerHTML="";
+    document.getElementById("executandoProjeto").innerHTML = `<label id="forprojeto">Projeto:</label>
+                                                              <input id="nomearProjeto"/>
+                                                              <button id="novoprojeto" onclick="insertProjeto()">Inserir</button>`;
+}
+function insertProjeto(){
+    let nomear = document.getElementById("nomearProjeto").value;
+    $.ajax({
+        dataType: "json",
+        url: "log/gamelist.json",
+        type: 'GET',
+        success: function(data){
+            data.push(nomear);
+            console.log(data);
+            document.getElementById("executandoProjeto").innerHTML="";
+            require_gamelist();
+        }
+      });
+    }
 function require_gamelist(){
 $.ajax({
     dataType: "json",
@@ -8,8 +30,8 @@ $.ajax({
         for(i in data){
             opt+=`<option value="${data[i]}">${data[i]}</option>`;
         }
-        sel =`<label id="forprojeto">Projeto:</label><select id="projeto">${opt}</select><button id="novoprojeto" onclick="criarProjeto()">Novo</button>`;
-        document.getElementById(`apresentation`).innerHTML+=sel;
+        sel =`<label id="forprojeto">Projeto:</label><select id="projeto">${opt}</select><button id="novoprojeto" onclick="criarProjeto()">Novo</button></div>`;
+        document.getElementById(`executandoProjeto`).innerHTML+=sel;
     }
   });
 }
@@ -24,7 +46,7 @@ function insertion(){
     }
     select =`<label id="forpositionx">Posição X:</label><select id="positionx">${opcoesx}</select>`;
     select +=`<label id="forpositiony">Posição y:</label><select id="positiony">${opcoesy}</select>`;
-    document.getElementById(`apresentation`).innerHTML+=select;
+    document.getElementById(`apresentation`).innerHTML=`<div id="executandoProjeto"></div>${select}`;
 }
 $(document).ready(function(){
     require_gamelist();
